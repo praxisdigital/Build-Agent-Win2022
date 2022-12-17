b455ec2a0fe627d2409338874f436e820865be11
Jan Madsen <93516348+jmapraxis@users.noreply.github.com>
2022-12-17 21:19:37 +0100

    Update AgentInstall.ps1

--- AgentInstall.ps1
+++ AgentInstall.ps1
@@ -2,6 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('http://internal/odata/repo/ChocolateyInstall.ps1'))
 
 # InstallSoftware
-choco install dotnet -y
+choco install dotnet-sdk -y
 choco install nodejs-lts -y
 choco install git -y
+choco install windows-sdk-10.1 -y
